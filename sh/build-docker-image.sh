b86f30db77fb177e0b09163e9f157438b8fe480c
Juarez Sampaio <jz@datarisk.io>
2021-11-23 17:39:35 -0300

    Add details on building docker image

---
+++ sh/build-docker-image.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# clear the repo from non commited changes
+git clean -xdf
+
+# install dotnet dependencies
+dotnet tool restore \
+    && dotnet paket install
+
+# stop and clean all docker containers
+docker stop $(docker ps -a -q) && docker system prune -f && docker volume prune -f
+
+# start dev and compile time database
+docker-compose -f docker-compose.yml up -d runtime-database compile-database
+
+sh/migrate.sh
+
+dotnet publish -c Release -o dotnet-build
+
+SHORT_SHA=$(git rev-parse --short=16 HEAD)
+LAST_COMMIT_DT=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%Y.%m.%d-%H.%M.%S)
+DEPLOYMENT_TAG=sqlprovider-demo:${LAST_COMMIT_DT}
+
+docker build -f Dockerfile . -t ${DEPLOYMENT_TAG}
